a9d72b272ec2b91ce7a7c1637de9863dc78481dc
0x00-sys <71561164+0x00-sys@users.noreply.github.com>
2026-07-16 15:52:43 +0200

    PermissionsViewer: make View Role Members work without BetterRoleContext

--- src/plugins/betterRoleContext/index.tsx
+++ src/plugins/betterRoleContext/index.tsx
@@ -86,6 +86,10 @@ type RoleMemberPopout = ComponentType<RoleMemberPopoutProps>;
 
 let RoleMemberPopout: RoleMemberPopout = () => null;
 
+export function setRoleMemberPopout(component: RoleMemberPopout) {
+    RoleMemberPopout = component;
+}
+
 export function buildExtraRoleContextMenuItems(role: Role, guild: Guild, popoutRef?: React.RefObject<any>) {
     if (!role) return { before: [], after: [] };
 
@@ -215,7 +219,7 @@ export default definePlugin({
             find: ".ROLE_MENTION)",
             replacement: {
                 match: /function (\i)(?=.+?renderPopout:.{0,20}\1,\{guildId:\i,channelId:\i)/,
-                replace: "$self.RoleMembers=$1;$&"
+                replace: "$self.setRoleMemberPopout($1);$&"
             }
         },
         // Conflicts with RoleColorEverywhere which changes the code at the end of our match. (and also uses same find & similar match)
@@ -234,9 +238,7 @@ export default definePlugin({
         DeveloperMode.updateSetting(true);
     },
 
-    set RoleMembers(component: RoleMemberPopout) {
-        RoleMemberPopout = component;
-    },
+    setRoleMemberPopout,
 
     contextMenus: {
         "dev-context"(children, { id }: { id: string; }) {
